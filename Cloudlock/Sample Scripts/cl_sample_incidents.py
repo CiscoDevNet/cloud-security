f3f10ff2aedecd88129492d8d7c2fe7e11a8bc3c
Yaron Caspy <tftd@users.noreply.github.com>
2019-04-11 17:58:27 +0300

    Added copyright notice

--- Cloudlock/Sample Scripts/cl_sample_incidents.py
+++ Cloudlock/Sample Scripts/cl_sample_incidents.py
@@ -1,6 +1,21 @@
 #! /usr/bin/python
 """
-A simple demo of connecting and working with the CloudLock API.
+This is a simple demo of connecting and working with the CloudLock API.
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
 """
 
 import argparse
