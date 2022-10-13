0e48a27fd9dcd57bb41582195aea5fae9af51881
Cody Cardinal <85251941+ccardina@users.noreply.github.com>
2022-10-13 09:41:42 -0700

    Postman Updates and OpenAPI Updates for NSD_Recheck Script! (#26)
    * README.md: Postman JSON URL and steps updated.
    
    * Updated NSD_Recheck script and docs for OpenAPI!
    
    * Updated README.md to reference legacy api version
    
    * Update nsd_recheck.py
    
    * Update nsd_recheck.py
    
    * Legacy Folder Re-do and OpenAPI Token updates!
    
    * Updates after internal review!
    
    * Updated Readme Files

--- Umbrella/Samples/SOCTools/NSD_Recheck/README.md
+++ Umbrella/Samples/SOCTools/NSD_Recheck/README.md
@@ -2,55 +2,64 @@
 
 Newly Seen Domains (NSDs) are domains that are new or unknown to the Umbrella DNS resolvers. You can set an Umbrella DNS Policy to block the Newly Seen Domains classification. Blocking NSDs improves the security of your networks.
 
-## Maintain an NSD Exemption List
 
-When you choose to block a NSD, you may occasionally need to temporarily allow a domain. Allowed domains are no longer checked for future security classification. Because NSD categorization expires after a few days, you must not leave domains on your NSD allow list. The NSDs Recheck (`nsd_recheck.py`) script helps you to automate the maintenance of an Umbrella destination list that contains NSDs.
+## Updates ( Oct 5, 2022)
 
-You can use the NSDs Recheck script to maintain an NSD exemption list. The NSD Recheck script integrates with the Umbrella Investigate API and Management API to manage the NSDs in your destination list. 
+This script is now updated for the new Umbrella OpenAPI! If you are using the Legacy Umbrella Management API for your Destination Lists, click [here](https://github.com/CiscoDevNet/cloud-security/blob/master/Umbrella/Samples/SOCTools/NSD_Recheck/LegacyAPI_README.md) for the Legacy API Version. 
+
+
+## Maintain a Newly Seen Domain Exemption List
+
+You may need to temporarily allow domains that are categorized as an NSD. Umbrella does not check allowed domains for future security classification. It is important that exemptions are removed so regular Umbrella protection can apply if the domain ever changes. Because NSD categorization typically expires after a few days, we recommend keeping your NSD exemption list trimmed. The NSDs Recheck (`nsd_recheck.py`) script helps you to automate that maintenance for an Umbrella destination list that contains NSDs. The NSD Recheck script integrates with the Umbrella Investigate API and Open API to manage the NSDs in your destination list. 
+
 
 The NSDs Recheck script:
 
-* Uses the Umbrella Management API to query all domains on a specific destination list.
+* Uses the Umbrella Open API to query all domains on a specific destination list.
 * Uses the Umbrella Investigate API to check all domains on the destination list.
-* Removes any domains blocked by Cisco Umbrella with a security classification of: malware, phishing, command and control.
-* Removes all domains except those still classified as NSD.
+* Removes any domains blocked by Cisco Umbrella with a security classification of malware, phishing, or command and control.
+* Removes all other domains except for those still classified as NSD.
 
 We recommended that you run the `nsd_recheck.py` script regularly and automate maintenance of your NSD allow list.
 
-**Note:** You should only run the `nsd_recheck.py` script on a custom allow list that contains only NSDs. Do not use this script on an allow list containing domains for any other exemption reasons.
+**Note:** You should run the `nsd_recheck.py` script on a custom allow list that contains **only** newly seen domains. Do not use this script on an allow list containing domains for any other exemption reasons.
+
 
 ## Prerequisites
 
-* You must have a subscription to the Cisco Umbrella Investigate API to use the Umbrella Investigate API.
+* You must have a subscription to the Cisco Umbrella that includes use of Umbrella Investigate API.
 The Umbrella Investigate API license is an add-on package and is not included in the Umbrella Advantage or Essentials Packages. For more information, see [Umbrella Investigate](https://umbrella.cisco.com/products/umbrella-investigate).
 * Python 3.x
+
 
 ## Step 1: Create Umbrella API Keys
 
 * Create an Umbrella Investigate API token. For more information, see [Create Umbrella Investigate API Token.](https://developer.cisco.com/docs/cloud-security/#!investigate-getting-started)
-* Create an Umbrella Management API key and secret. For more information, see [Create Umbrella Generate Key and Secret](https://developer.cisco.com/docs/cloud-security/#!getting-started-overview).
+* Create an Umbrella Open API key and secret. For more information, see [Create Umbrella Generate Key and Secret](https://developer.cisco.com/docs/cloud-security/#!authentication/create-an-api-key).
+* Assign this Umbrella Open API key the permission `policies:read,write`.
+
 
 ## Step 2: Set Environment Variables
 
 In your system, export the environment variables used by the **NSDs Recheck** script.
 
-* `ORG_ID`—Your organization ID. For more information, see [Find Your Organization ID](https://developer.cisco.com/docs/cloud-security/#!getting-started-overview/get-organization-information).
-* `MANAGEMENT_KEY`—Your Umbrella Management API key.
-* `MANAGEMENT_SECRET`—Your Umbrella Management API secret.
+* `API_KEY`—Your Umbrella Open API key.
+* `API_SECRET`—Your Umbrella Open API secret.
 * `INVESTIGATE_TOKEN`—Your Umbrella Investigate API token.
 
-Set up the environment variables for the `nsd_recheck.py` script in your `.bash_profile`:
+Set up the environment variables for the `nsd_recheck.py` script in your `.bash_profile` or Windows Environment Variable:
 
 ```bash
-export ORG_ID="insert your Umbrella organization ID"
-export MANAGEMENT_KEY="insert your Umbrella Management API key"
-export MANAGEMENT_SECRET="insert your Umbrella Management API secret"
+export API_KEY="insert your Umbrella Open API key"
+export API_SECRET="insert your Umbrella Open API secret"
 export INVESTIGATE_TOKEN="insert your Umbrella Investigate API token"
 ```
 
+
 ## Step 3: Create an Umbrella Destination List
 
-* You must create an Umbrella destination list with the `access` type of `allow` and `dns`. Add NSDs that you do not want blocked to your allow destination list. For more information, see [Create an Allow list](https://docs.umbrella.com/deployment-umbrella/docs/add-a-new-destination-list).
+* You must create an Umbrella destination list with the `access` type of `allow` and `dns`. Add NSDs that you do not want blocked to this allow destination list. For more information, see [Create an Allow list](https://docs.umbrella.com/deployment-umbrella/docs/add-a-new-destination-list).
+
 
 ## Step 4: Run the NSDs Recheck Script
 
--- Umbrella/Samples/SOCTools/NSD_Recheck/nsd_recheck.py
+++ Umbrella/Samples/SOCTools/NSD_Recheck/nsd_recheck.py
@@ -18,69 +18,94 @@ or implied.
     Used to maintain an allow list used only for NSD temporary exemptions.
 '''
 
-import requests, json
-import base64
+
+
+
+import requests
+import json
 import pandas as pd
 import os
 import sys
+import time
 
-''' Generates URL for Management API, including org_id and dest_id '''
-def gen_management_api_url(org_id, dest_id):
-    management_api_url = (
-            "https://management.api.umbrella.com/v1/organizations/"
-            + org_id
-            + "/destinationlists/"
-            + dest_id
-            + "/destinations"
+
+class UmbrellaAPI():
+    def __init__(self, token_url, client_id, client_secret):
+        self.token_url = token_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+        try:
+            self.access_token = self.getAccessToken()
+            if self.access_token is None:
+                    raise Exception("Request for access token failed")
+        except Exception as e:
+            print(e)
+            
+    def getAccessToken(self):
+        try:
+            p={'grant_type': 'client_credentials'}
+            api_headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            rsp = requests.post(self.token_url, data=p, headers=api_headers, auth=(self.client_id, self.client_secret))
+            rsp.raise_for_status()
+        except Exception as e:
+            print(e)
+            return None
+        else:
+            clock_skew = 300
+            self.access_token_expiration = int(time.time()) + rsp.json()['expires_in'] - clock_skew
+            return rsp.json()['access_token']
+
+    def __str__(self):
+        return self.access_token
+
+
+def gen_open_api_url(dest_id):
+    ''' Generates URL for Open API, including dest_id '''
+    open_api_url = (
+        "https://api.umbrella.com/policies/v2/destinationlists/"
+        + dest_id
+        + "/destinations"
     )
-    return management_api_url
+    return open_api_url
 
 
-''' Generates base64 encoded key and secret for passing credentials to the management api '''
-def gen_credentials(mkey, msec):
-    """Gen Base64 API Token"""
-    mkp = mkey + ":" + msec
-    management_api_pass = base64.b64encode(mkp.encode()).decode()
-    return management_api_pass
-
-'''GET Request destinations from Umbrella API'''
-def get_domains(management_api_pass, management_api_url):
-    headers = {"Authorization": "Basic " + management_api_pass, "Content-Type": "application/json"}
-    payload = None
-    destinations = requests.request("GET", management_api_url, headers=headers, data=payload)
-    if destinations:
-        destinations = pd.DataFrame.from_dict(destinations.json()['data'])
-    return destinations
+def get_domains(open_api_token, open_api_url):
+    '''GET Request destinations from Umbrella API'''
+    h = {"Authorization": "Bearer " + open_api_token}
+    p = {}
+    r = requests.request("GET", open_api_url, headers=h, data=p).json()
+    return pd.DataFrame.from_dict(r['data'])
 
 
-''' Remove Domains from the Umbrella destination list.
-    Domains are checked by check_for_nsd() and check_for_blocks() functions. '''
-def remove_domains(domain_id):
-    deleteurl = management_api_url + "/remove"
-    headers = {
-        "Authorization": "Basic " + management_api_pass,
+def remove_domains(domain_id, open_api_token):
+    ''' Remove Domains from the Umbrella destination list.'''
+    url = open_api_url + "/remove"
+    h = {
+        "Authorization": "Bearer " + open_api_token,
         "Accept": "application/json",
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
     }
-    payload = json.dumps(domain_id)
-    removal_response = requests.request("DELETE", deleteurl, headers=headers, data=payload).json()
-    return removal_response
+    p = json.dumps(domain_id)
+    r = requests.request("DELETE", url, headers=h, data=p).json()
+    return r
 
-'''Check Categorization and Classification of Domains with the Investigate API'''
+
 def check_domains(domains):
+    '''Check Categorization and Classification of Domains with the Investigate API'''
     investigate_url = "https://investigate.api.umbrella.com/domains/categorization"
-    payload = json.dumps(domains)
+    p = json.dumps(domains)
     investigate_headers = {
-        "Authorization": "Bearer " + ipass,
+        "Authorization": "Bearer " + i_pass,
         "Content-Type": "application/json",
     }
-    check_request = requests.request("POST", investigate_url, headers=investigate_headers, data=payload)
+    check_request = requests.request("POST", investigate_url, headers=investigate_headers, data=p)
     checked = check_request.json()
     return checked
 
 
-'''Use check_domains data to see if blocked and return a list of domain IDs to be removed.'''
 def check_for_blocks(checked):
+    '''Use check_domains data to see if blocked and return a list of domain IDs to be removed.'''
     blocked_domains = []
     for d in checked:
         if checked.get(d)["status"] == -1:
@@ -92,10 +117,10 @@ def check_for_blocks(checked):
     return block_ids
 
 
-'''Checks if any domains are no longer NSD, returning a list of expired NSD domain IDs to be removed.'''
 def check_for_nsd(checked):
+    '''Checks if any domains are no longer NSD, returning a list of expired NSD domain IDs to be removed.'''
     expired = []
-    nsd = "108" # Investigate API Identifies newly seen domains with classifier # 108.
+    nsd = "108"  # Investigate API Identifies newly seen domains with classifier # 108.
     for d in checked:
         if nsd not in checked.get(d)["security_categories"]:
             expired.append(d)
@@ -111,35 +136,42 @@ if __name__ == '__main__':
 
     print("Starting Newly Seen Domains Re-Check Script.")
 
-    # dest_id = 12345678 # ( Optionally hard-set the Destination List ID for automation, comment out 126 )
+    # dest_id = 12345678 # Option to Hardset Destination List ID for automation, comment out line 156 )
     destinations = []  # reset the get request
     delete_request = []  # reset the delete request
     domains = []  # reset the domains list.
 
     # Set these variables in your environment or .bash_profile. Check the README for more information.
-    mkey = os.environ["MANAGEMENT_KEY"]  # Umbrella Management API key
-    msec = os.environ["MANAGEMENT_SECRET"]  # Umbrella Management API secret
-    ipass = os.environ["INVESTIGATE_TOKEN"]  # Umbrella Investigate API token
-    orgid = os.environ["ORG_ID"]  # orgID
+    i_pass = os.environ["INVESTIGATE_TOKEN"]  # Umbrella Investigate API token
+    token_url = os.environ.get('TOKEN_URL') or 'https://api.umbrella.com/auth/v2/token'
+    client_id = os.environ.get('API_KEY')  # Umbrella Open API key
+    client_secret = os.environ.get('API_SECRET')  # Umbrella Open API secret
+    
+    # Exit out if the client_id or client_secret is not set
+    for var in ['API_SECRET', 'API_KEY', 'INVESTIGATE_TOKEN']:
+        if os.environ.get(var) == None:
+            print("Required environment variable: {} not set".format(var))
+            sys.exit(1)
 
     # Enter destination list ID
-    dest_id = input("Enter your destination list ID for NSD recheck: ")
+    dest_id = input("Enter your NSD Allow Destination List ID (1234567) for NSD recheck: ")
     print("Destination list ID: ", dest_id)
+    
+    # Retrieve Open API Token
+    token = UmbrellaAPI(token_url, client_id, client_secret)
+    open_api_token = token.access_token
 
-    # generate credentials and a Management API URL containing orgId and destination list ID
-    management_api_pass = ""
-    management_api_pass = gen_credentials(mkey, msec)
-    management_api_url = ""
-    management_api_url = gen_management_api_url(orgid, dest_id)
+    # Generate Open API URL containing destination list ID
+    open_api_url = gen_open_api_url(dest_id)
 
     # Get the list of domains from the destination list then prepare them. Stops script if list of domains is empty.
-    destinations = get_domains(management_api_pass, management_api_url)
-
+    destinations = get_domains(open_api_token, open_api_url)
+    
     if len(destinations) == 0:  # Stop Script if the list is empty
-        sys.exit("No Domains on list. Stopping Script")
+        print("No Domains on list. Stopping Script")
     elif len(destinations) > 0:
         '''If the list has domains, prepare the list of domain IDs, check each domain ID with the
-        Investigate API for malware and NSD expiration.'''
+        Investigate API for security blocks and NSD expiration.'''
         domains = destinations.destination
         domains = domains.tolist()
         print(f"Checking {len(domains)} Domains")
@@ -147,16 +179,17 @@ if __name__ == '__main__':
         block_ids = check_for_blocks(checked)
         expired_ids = check_for_nsd(checked)
 
-        # Remove Each Malware Domain ID from the destination List ( Malware was allowed )
-        print(f"Removing {len(block_ids)} domains that are blocked.")
+        # Remove Each Blocked Domain ID from the destination list, because the block would be allowed.
         if len(block_ids) > 0:
-            removal_response = remove_domains(block_ids)
+            print(f"Removing {len(block_ids)} domains that are blocked.")
+            removal_response = remove_domains(block_ids, open_api_token)
             print('Result :' + str((removal_response['status'])))
         # Remove Domains Not Marked NSD from the Destination List because NSD classification has expired.
-        print(f"Removing {len(expired_ids)} expired NSDs.")
         if len(expired_ids) > 0:
-            removal_response = remove_domains(expired_ids)
+            print(f"Removing {len(expired_ids)} expired NSDs.")
+            removal_response = remove_domains(expired_ids, open_api_token)
             print('Result : ' + str((removal_response['status'])))
 
-    print("Domains remaining for next run : " + str(removal_response['data']['meta']['destinationCount']))
+        print("Domains remaining for next run : " + str(removal_response['data']['meta']['destinationCount']))
+
     print("Done.")
